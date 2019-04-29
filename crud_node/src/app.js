e256f98aa80824c15893a7255af1cf86d2060aa9
Centro de Computo Dispositivos Moviles <ccdmadm@iMac-de-Centro-6.local>
2019-04-29 12:08:11 -0600

    Conexion a la base de datos

--- crud_node/src/app.js
+++ crud_node/src/app.js
@@ -1,12 +1,32 @@
+const path = require ('path');
+const morgan=require('morgan');
 const express= require('express');
+const mysql= require ('mysql');
+const myConnection= require ('express-myconnection');
 const app= express();
 //Importar rutas
 const indiceRutas=require('./rutas/index');
 
+//Configuraciones
+app.set('port',process.env.PORT || 3000)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
 //usamos las rutas
 app.use("/",indiceRutas);
 
+//Middleware
+app.use(morgan('de'));//dev= desarrollo
+app.use(myConnection(mysql,{
+
+     host: 'localhost',
+     user: 'root',
+     password: '',
+     port: 3306,
+     database : 'crudnodejsmysql'
+}, 'single'));
+
 //Activar el servidor de escucha
-app.listen(3000, ()=> {
-	console.log("Escuchando en puerto 3000, jejeje");
+app.listen(app.get('port'),()=> {
+	console.log("Escuchando en puerto 3000");
 })
