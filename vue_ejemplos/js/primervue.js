e748ae1d606c2d0c5085379ba326bd475dbf1750
Centro de Computo Dispositivos Moviles <ccdmadm@iMac-de-Centro-6.local>
2019-05-07 12:06:21 -0600

    Primer VUE

---
+++ vue_ejemplos/js/primervue.js
@@ -0,0 +1,12 @@
+const app= new Vue({
+	el: "#app",
+	data: {
+		titulo: "Hola mundo con VUE",
+		frutas: ['manzanas', 'peras','platano','mango'],
+		frutas2: [
+       {nombre: 'manzana', cantidad: 10},
+       {nombre: 'peras', cantidad: 0},
+       {nombre: 'platano', cantidad: 20},
+       {nombre: 'mango', cantidad: 80},]
+	} 
+ });
