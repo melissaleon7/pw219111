849dc07c046e9b2832a58bff9ff8335af6eadafe
Centro de Computo Dispositivos Moviles <ccdmadm@iMac-de-Centro-6.local>
2019-05-09 11:51:57 -0600

    Agregar frutas con vue

--- vue_ejemplos/js/primervue.js
+++ vue_ejemplos/js/primervue.js
@@ -7,6 +7,32 @@ const app= new Vue({
        {nombre: 'manzana', cantidad: 10},
        {nombre: 'peras', cantidad: 0},
        {nombre: 'platano', cantidad: 20},
-       {nombre: 'mango', cantidad: 80},]
-	} 
+       {nombre: 'mango', cantidad: 80}
+       ],
+       nuevaFruta: '',
+       total: 0
+	} ,
+	methods: {
+		agregarFruta(){
+			//console.log("Diste clic");
+			if(this.nuevaFruta!= ''){
+				this.frutas.push(this.nuevaFruta);
+			this.frutas2.push({
+				nombre: this.nuevaFruta,
+				cantidad:0
+			});
+			this.nuevaFruta=''
+			}
+			
+		}
+	},
+	computed: {
+		sumarFrutas(){
+			this.total=0;
+			for(fruta of this.frutas2){
+				this.total= this.total+fruta.cantidad;
+                 }
+                 return this.total;
+			}
+	}
  });
